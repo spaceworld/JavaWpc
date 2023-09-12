4dbbfc30b396fa577a769c59a53df747b76e81fd
spaceworld <144647758+spaceworld@users.noreply.github.com>
2023-09-12 17:57:36 +0900

    Update Chapter05.java

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -1,1 +1,14 @@
+public class Chapter05 {
 
+	public static void main(String[] args) {
+		String str1;  // String型の変数str1を宣言
+    String str2;  // String型の変数str2を宣言
+    str1 = "SanaeMizokami";  //str1を初期化
+    str2 = "ベアアップ";  //str2を初期化
+    System.out.println(str1); //str1の内容を出力
+		System.out.println(str2); //str2の内容を出力
+    final int luckyNumber = 03;
+    System.out.println(luckyNumber); //num1の内容を出力
+
+	}
+}
